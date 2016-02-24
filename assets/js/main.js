16b002031166abb64d6b078901cc42d9be490012
Stjepan Grgic <grgic.stjepan@gmail.com>
2016-02-25 00:55:24 +0100

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,4 +14,7 @@ function addShowRightMenu(elem) {
   elem.classList.toggle('menu-icon--active');
   var mainNav = document.querySelector('.main');
   mainNav.classList.toggle('mobile-nav');
+
+  var hideContainer = document.querySelector('body');
+  hideContainer.classList.toggle('hide');
 }
